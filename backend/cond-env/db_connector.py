6364a3eae8a0cabd3ae9931eec7c3df054a16ac8
romeodombaj <romeodombaj@gmail.com>
2023-08-15 09:34:02 +0200

    kreiranje osnovne baze podataka, tablice i get/post funkcije

--- backend/cond-env/db_connector.py
+++ backend/cond-env/db_connector.py
@@ -10,3 +10,30 @@ class Receipt(DB.Entity):
 
 DB.bind(provider="sqlite", filename="database.sqlite", create_db=True)
 DB.generate_mapping(create_tables=True)
+
+
+@orm.db_session
+def add_receipt(receipt, description, price=None):
+    try:
+        with orm.db_session:
+            Receipt(receipt=receipt, description=description, price=price)
+            response = {"response":"Success"}
+            return response
+    except Exception as e:
+        return {"response":"Error","error":e}
+        
+
+def get_receipt(receipt_to_get):
+    try:
+        with orm.db_session:
+            result = Receipt.get(receipt=receipt_to_get).to_dict()
+            response = {"response":"Success", "data":result}
+            return response
+    except Exception as e:
+        return {"response":"Error","error":e}
+
+if __name__ == "__main__":
+    #res= add_receipt("krumpir", "jako pečeni krumpir u pećnici")
+    #print(res)
+    res=get_receipt("krumpir")
+    print(res)
